bff4c3362123b91a0b65c5714e6c8a9d08b000ad
ABHIRUP CHOWDHURY <108288399+Abhirup898@users.noreply.github.com>
2025-03-05 23:55:23 +0530

    Update app_booking.py

--- app_booking.py
+++ app_booking.py
@@ -42,5 +42,8 @@ def book_slot():
         return jsonify({"error": str(e)}), 500
 
 # Run the app
+
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+   
